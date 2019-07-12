a4f5aaeeecc48c7fcf10472dc1017a5c8b6318f9
Bogomolov Mikhail <mocrobs@gmail.com>
2019-07-12 20:15:30 +0700

    may be refresh tabs

--- app/src/main/java/ru/ftc/android/shifttemple/features/tasks/presentation/SampleFragmentPagerAdapter.java
+++ app/src/main/java/ru/ftc/android/shifttemple/features/tasks/presentation/SampleFragmentPagerAdapter.java
@@ -11,6 +11,9 @@ public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
     final int PAGE_COUNT = 3;
     private String tabTitles[] = new String[] { "Взять", "Взятые", "Созданные" };
     private Context context;
+    private TaskFragment bf1 = new TaskFragment(0);
+    private TaskFragment bf2 = new TaskFragment(1);
+    private TaskFragment bf3 = new TaskFragment(2);
 
     public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
         super(fm);
@@ -26,13 +29,58 @@ public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                TaskFragment bf1 = new TaskFragment(position);
+                try {
+                    bf1.getActivity().getSupportFragmentManager().beginTransaction().remove(bf1).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                try {
+                    bf2.getActivity().getSupportFragmentManager().beginTransaction().remove(bf2).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                try {
+                    bf3.getActivity().getSupportFragmentManager().beginTransaction().remove(bf3).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                bf1 = new TaskFragment(position);
                 return bf1;
             case 1:
-                TaskFragment bf2 = new TaskFragment(position);
+                try {
+                    bf1.getActivity().getSupportFragmentManager().beginTransaction().remove(bf1).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                try {
+                    bf2.getActivity().getSupportFragmentManager().beginTransaction().remove(bf2).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                try {
+                    bf3.getActivity().getSupportFragmentManager().beginTransaction().remove(bf3).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                bf2 = new TaskFragment(position);
                 return bf2;
             case 2:
-                TaskFragment bf3 = new TaskFragment(position);
+                try {
+                    bf1.getActivity().getSupportFragmentManager().beginTransaction().remove(bf1).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                try {
+                    bf2.getActivity().getSupportFragmentManager().beginTransaction().remove(bf2).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                try {
+                    bf3.getActivity().getSupportFragmentManager().beginTransaction().remove(bf3).commit();
+                }
+                catch (NullPointerException npe){
+                }
+                bf3 = new TaskFragment(position);
                 return bf3;
             default:
                 return null;
